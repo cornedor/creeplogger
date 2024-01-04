5b37f323d99089f239af80e9a1ee8ac51a021c4a
Corné Dorrestijn <contact@corne.info>
2024-01-04 13:13:51 +0100

    Add auth for cron

--- src/app/api/daily-update/route.ts
+++ src/app/api/daily-update/route.ts
@@ -2,6 +2,13 @@ import { sendDailyUpdate } from "@/helpers/Mattermost.bs.mjs";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
   if (request.nextUrl.searchParams.get("send")) {
     await sendDailyUpdate();
     return NextResponse.json({ ok: true });
