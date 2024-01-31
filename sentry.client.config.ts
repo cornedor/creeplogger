4bbbf0e3f962644027c58fa7ba5a78c2f9e8c1ed
Corné Dorrestijn <contact@corne.info>
2024-01-31 12:06:21 +0100

    Lower sampling rate

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -8,16 +8,16 @@ Sentry.init({
   dsn: "https://ccdb445dc99c2731c88db4f41e53e158@o4506552216453120.ingest.sentry.io/4506552218615808",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.2,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
   replaysOnErrorSampleRate: 1.0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // This sets the sample rate to be 4%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0.04,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
